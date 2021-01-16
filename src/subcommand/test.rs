54b3fb0f3d78749d454223e6c79cc413e059126a
yscurea <yuuki.990510@gmail.com>
2021-01-16 19:19:30 +0900

    fix print format

--- src/subcommand/test.rs
+++ src/subcommand/test.rs
@@ -1,3 +1,4 @@
+#![allow(unused_imports)]
 use super::utils;
 use core::panic;
 use std::io::Read;
@@ -10,6 +11,8 @@ use subprocess::Redirection;
 use subprocess::{Exec, Popen, PopenConfig};
 use wait_timeout::ChildExt;
 
+#[derive(PartialEq)]
+#[allow(dead_code)]
 pub enum ProblemResultType {
     AC,  // accepted
     CE,  // compile error
@@ -20,6 +23,7 @@ pub enum ProblemResultType {
 }
 
 pub struct ProblemResult {
+    problem_path: String,
     result_type: ProblemResultType,
     input: String,
     user_output: String,
@@ -28,36 +32,57 @@ pub struct ProblemResult {
 
 impl ProblemResult {
     pub fn print(&self) {
+        utils::std_output::print_info(
+            utils::std_output::PrintColor::BLUE,
+            "INFO",
+            format!("case - {}:", self.problem_path).as_str(),
+        );
         match self.result_type {
             ProblemResultType::AC => {
-                println!("AC");
+                let message =
+                    utils::std_output::color_print(utils::std_output::PrintColor::GREEN, "AC");
+                utils::std_output::print_info(
+                    utils::std_output::PrintColor::GREEN,
+                    "SUCCESS",
+                    message.as_str(),
+                );
             }
             ProblemResultType::WA => {
-                println!("WA");
+                let message =
+                    utils::std_output::color_print(utils::std_output::PrintColor::RED, "WA");
+                utils::std_output::print_info(
+                    utils::std_output::PrintColor::RED,
+                    "FAILURE",
+                    message.as_str(),
+                );
+                println!("input:\n{}", self.input);
+                println!("output: \n{}", self.user_output);
+                println!("expected:\n{}", self.expected_output);
             }
             ProblemResultType::TLE => {
-                println!("TLE");
+                let message =
+                    utils::std_output::color_print(utils::std_output::PrintColor::YELLOW, "TLE");
+                utils::std_output::print_info(
+                    utils::std_output::PrintColor::RED,
+                    "FAILURE",
+                    message.as_str(),
+                );
+                println!("input:\n{}", self.input);
+                println!("output: \n{}", self.user_output);
+                println!("expected:\n{}", self.expected_output);
             }
             _ => {
                 println!("error");
             }
         }
-        println!("input:\n{}", self.input);
-        println!("output: none");
-        println!("expected:\n{}", self.expected_output);
+        println!("");
     }
 }
 
-pub struct ProblemsResult {
-    ac_num: i8,
-    problems_result: Vec<ProblemResult>,
-}
-
-pub fn test_code() -> Result<(), Box<dyn std::error::Error>> {
+pub fn test_code() -> Result<(), ()> {
     let test_dir = "test";
     let test_files = fs::read_dir(test_dir).unwrap();
-    let mut problem_num = 0;
-    let mut ac_num = 0;
+    let mut result_list: Vec<ProblemResult> = Vec::new();
     for test_file in test_files {
         let path_name = format!("{}", test_file.unwrap().path().display());
         let split_path_name: Vec<&str> = path_name.split('.').collect();
@@ -65,14 +90,8 @@ pub fn test_code() -> Result<(), Box<dyn std::error::Error>> {
         if split_path_name[1] == "out" {
             continue;
         }
-        problem_num += 1;
-        utils::std_output::print_info(
-            utils::std_output::PrintColor::BLUE,
-            "INFO",
-            format!("case - {}:", file_name_without_extension).as_str(),
-        );
         // テストディレクトリから標準入力用ファイルを取得する
-        let input_string = fs::read(format!("{}.in", file_name_without_extension))
+        let std_input = fs::read(format!("{}.in", file_name_without_extension))
             .unwrap()
             .iter()
             .map(|&s| s as char)
@@ -83,84 +102,68 @@ pub fn test_code() -> Result<(), Box<dyn std::error::Error>> {
             .stdout(Stdio::piped())
             .spawn()
             .unwrap();
-
         {
             let stdin = subprocess.stdin.as_mut().expect("failed to get stdin");
             stdin
-                .write_all(input_string.as_bytes())
+                .write_all(std_input.as_bytes())
                 .expect("failed to write to stdin");
         }
-
         let is_timeout: bool;
-
-        match subprocess.wait_timeout(Duration::new(2, 0)).unwrap() {
-            Some(status) => {
+        let wait_time = Duration::new(2, 0);
+        match subprocess.wait_timeout(wait_time).unwrap() {
+            Some(_) => {
                 is_timeout = false;
-                status.code()
             }
             None => {
                 is_timeout = true;
                 subprocess.kill().unwrap();
-                subprocess.wait().unwrap().code()
             }
         };
-
         let mut user_ans = String::new();
         subprocess
             .stdout
             .unwrap()
             .read_to_string(&mut user_ans)
             .unwrap();
-
-        let correct_ans = fs::read(format!("{}.out", file_name_without_extension))
+        let expected_ans = fs::read(format!("{}.out", file_name_without_extension))
             .unwrap()
             .iter()
             .map(|&s| s as char)
             .collect::<String>();
-
-        if user_ans == correct_ans {
-            ac_num += 1;
-            let message =
-                utils::std_output::color_print(utils::std_output::PrintColor::GREEN, "AC");
-            utils::std_output::print_info(
-                utils::std_output::PrintColor::GREEN,
-                "SUCCESS",
-                message.as_str(),
-            );
-        } else if is_timeout {
-            let message =
-                utils::std_output::color_print(utils::std_output::PrintColor::YELLOW, "TLE");
-            utils::std_output::print_info(
-                utils::std_output::PrintColor::RED,
-                "FAILURE",
-                message.as_str(),
-            );
-            println!("input:\n{}", input_string);
-            println!("output: none");
-            println!("expected:\n{}", correct_ans);
+        let mut result: ProblemResult = ProblemResult {
+            problem_path: path_name,
+            result_type: ProblemResultType::AC,
+            input: std_input,
+            user_output: user_ans.clone(),
+            expected_output: expected_ans.clone(),
+        };
+        if is_timeout {
+            result.result_type = ProblemResultType::TLE;
+        } else if user_ans == expected_ans {
+            result.result_type = ProblemResultType::AC;
         } else {
-            let message = utils::std_output::color_print(utils::std_output::PrintColor::RED, "WA");
-            utils::std_output::print_info(
-                utils::std_output::PrintColor::RED,
-                "FAILURE",
-                message.as_str(),
-            );
-            println!("input:\n{}", input_string);
-            println!("output:\n{}", user_ans);
-            println!("expected:\n{}", correct_ans);
+            result.result_type = ProblemResultType::WA;
+        }
+        result_list.push(result);
+    }
+    let mut ac_num = 0;
+    for problem_result in &result_list {
+        problem_result.print();
+        if problem_result.result_type == ProblemResultType::AC {
+            ac_num += 1;
         }
     }
-    if ac_num == problem_num {
+    if ac_num == result_list.len() {
         utils::std_output::print_info(
             utils::std_output::PrintColor::GREEN,
             "SUCCESS",
-            format!("{} AC / {} cases", ac_num, problem_num).as_str(),
+            format!("{} AC / {} cases", ac_num, result_list.len()).as_str(),
         );
     } else {
         utils::std_output::print_info(
             utils::std_output::PrintColor::RED,
             "FAILURE",
-            format!("{} AC / {} cases", ac_num, problem_num).as_str(),
+            format!("{} AC / {} cases", ac_num, result_list.len()).as_str(),
         );
     }
     return Ok(());
