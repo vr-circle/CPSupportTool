cc9d8160e450f467e7bf7ca4bb4905c4903c541d
yscurea <yuuki.990510@gmail.com>
2021-01-30 23:54:44 +0900

    add reqwest client

--- src/subcommand/login.rs
+++ src/subcommand/login.rs
@@ -1,6 +1,20 @@
+use reqwest;
+use reqwest::Response;
+
 use super::utils;
 
 pub fn login() -> Result<(), ()> {
     utils::std_output::print_info(utils::std_output::PrintColor::BLUE, "INFO", "login start");
+
+    let url = "https://atcoder.jp/";
+    let client = reqwest::Client::builder().build().unwrap();
+
     Ok(())
 }
+
+// async fn call_post_request(
+//     url: &str,
+//     params: &std::collections::HashMap<&str, String>,
+// ) -> Result<reqwest::Response, reqwest::Error> {
+//     Ok(())
+// }
