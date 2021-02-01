84fcee7ed44c1f4a2bf071f70d1b032af72ed328
yscurea <yuuki.990510@gmail.com>
2021-02-02 02:07:21 +0900

    fix get_csrf_token

--- src/subcommand/login.rs
+++ src/subcommand/login.rs
@@ -1,32 +1,43 @@
 use reqwest;
-use reqwest::Response;
 
 use super::utils;
 
-pub fn login() -> Result<(), ()> {
+pub fn login() -> Result<(), std::io::Error> {
     utils::std_output::print_info(utils::std_output::PrintColor::BLUE, "INFO", "login start");
 
     let login_url = "https://atcoder.jp/login";
 
     // username,password,csrf_tokenが必要
-
     // csrf_tokenは非ログイン状態でログインページにゲットリクエストを送ることで取得できる．
-    let parse_str = "input[name=\"csrf_token\"]";
+    let parse_str = r#"input[name="csrf_token"]"#;
     let selector = scraper::Selector::parse(parse_str).unwrap();
-    let body = reqwest::blocking::get(&login_url.to_string())?.text()?;
+    let body = reqwest::blocking::get(&login_url.to_string())
+        .unwrap()
+        .text()
+        .unwrap();
     let document = scraper::Html::parse_fragment(&body);
-    let csrf_token = document.select(&selector);
+    let mut csrf_token = "";
+    if let Some(element) = document.select(&selector).next() {
+        if let Some(token) = element.value().attr("value") {
+            csrf_token = token;
+        }
+    }
+
+    // How to input
+    let username = "";
+    let password = "";
+
+    let param = {
+        let mut params = std::collections::HashMap::new();
+        params.insert("username", username);
+        params.insert("password", password);
+        params.insert("csrf_token", csrf_token);
+        params
+    };
 
     // post reqwest
     // let client = reqwest::Client::new();
     // let mut res = client.post(logini_url).body(/*reqwest body */).send();
 
-    Ok(())
-}
-
-async fn call_post_request(
-    url: &str,
-    params: &std::collections::HashMap<&str, String>,
-) -> Result<reqwest::Response, reqwest::Error> {
-    Ok(())
+    return Ok(());
 }
